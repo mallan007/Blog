014a34a1aa01b377fe5ebd02cf74e980f1a06af4
mallan007 <allanqueiroz.eng@gmail.com>
2022-07-26 23:22:27 +0000

    1 e 2 posts com data e texto

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask("hello")
+
+posts =[
+    {
+        "title":"O meu primeiro Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Feulo",
+        "created": datetime(2022,7,25)
+    }, {
+        "title":"O meu primeiro Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Danilo",
+        "created": datetime(2022,7,26)
+    },
+]
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
